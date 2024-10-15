d4e8ada9963a644165d0784736d874080652f819
littlebean47 <jalynb48@gmail.com>
2024-10-15 14:11:59 -0400

    Added CORS + env variable

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const cors = require('cors')
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const PORT = process.env.PORT || 3001
 
 // Serve static files if needed (e.g., for production builds of React app)
 app.use(express.static('public'));
+app.use(cors());
 
 io.on('connection', (socket) => {
     // Receive and Send username to clients
@@ -26,6 +29,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3001, () => {
-    console.log('Server is running on port 3001');
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
